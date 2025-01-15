9787de1e878868a5e38bde238cc990ffe3c85837
Karthikeyan Natarajan <karthiknitt@gmail.com>
2025-01-15 20:00:13 +0530

    next initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,23 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render();
+import "./index.css";
+
+const Todo = (
+  <div className="container">
+    <div className="item">
+      <div className="date">
+        <div className="day">12</div>
+        <div className="month">Jan</div>
+      </div>
+      <div className="content">
+        <div className="title">
+          <h2>Learn React19</h2>
+        </div>
+        <div className="desc"> Study Lesson 11 on Codex.</div>
+      </div>
+    </div>
+  </div>
+);
+
+createRoot(document.getElementById("root")).render(Todo);
